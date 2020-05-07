6fe4fa74fa1edf86a233fba7776c1ae12512d69e
Smodi2007 <37974632+Smodi2007@users.noreply.github.com>
2020-05-07 12:27:09 +0530

    Update LIS_LCS.cpp

--- LIS_LCS.cpp
+++ LIS_LCS.cpp
@@ -4,12 +4,14 @@ using namespace std;
 /* lis() returns the length of the longest increasing 
 subsequence in arr[] of size n */
 
-int lis( int arr[], int n )  {  
-	int lis[n];  
+int lis( int arr[], int n )  
+{  
+    int lis[n];  
 	int maxValue, maxlis = 1;
 	
 	lis[0] = 1;  
-	for (int i = 1; i < n; i++ )  { 
+    for (int i = 1; i < n; i++ )  
+    { 
         maxValue = 0; 
         for (int j = 0; j < i; j++ )   
             if ( arr[j] < arr[i] )  
