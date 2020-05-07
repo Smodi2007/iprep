1f0d3a4eaef66cc2c88fe5c1b2f48aa1dbd8ab17
Smodi2007 <37974632+Smodi2007@users.noreply.github.com>
2020-05-07 18:56:48 +0530

    Update LIS_LCS.cpp

--- LIS_LCS.cpp
+++ LIS_LCS.cpp
@@ -33,6 +33,26 @@ int lis( int arr[], int n )
 **************************************************************/
 /* Dynamic Programming C++ implementation of LCS problem */
 /* Returns length of LCS for X[0..m-1], Y[0..n-1] */
+////WITH lcs[m+1][n+1] :Preferred.
+int lcs( char *X, char *Y, int m, int n )  {
+	int lcs[m+1][n+1];  
+    int i, j; 
+	//Building lcs[][].
+	//0th row represent as if length of X is zero ie. empty X i.e there is no X[0] at all.
+	//in this approach lcs[i][j] corresponds to X[i-1] & Y[j-1] ie lcs runs one index ahead of X/Y.
+    for (i = 0; i <= m; i++) {  
+        for (j = 0; j <= n; j++) {
+          	if (i == 0 || j == 0)  
+            	lcs[i][j] = 0;  
+			else if (X[i-1] == Y[j-1])  
+				lcs[i][j] = lcs[i - 1][j - 1] + 1;
+			else
+				lcs[i][j] = max(lcs[i - 1][j], lcs[i][j - 1]);  
+        } 
+    }  
+    return lcs[m][n];  
+}
+////WITH lcs[m][n]
 int lcs( char *X, char *Y, int m, int n )  
 {  
     int lcs[m][n];  
@@ -55,7 +75,6 @@ int lcs( char *X, char *Y, int m, int n )
 				lcs[i][j] = max(lcs[i - 1][j], lcs[i][j - 1]);  
         }  
     }  
-	
     return lcs[m-1][n-1];  
 }  
 
