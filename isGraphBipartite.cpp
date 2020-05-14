c43b9a831320510cae1de828baf69867f07daa4f
Smodi2007 <37974632+Smodi2007@users.noreply.github.com>
2020-05-14 21:49:16 +0530

    Create isGraphBipartite.cpp

---
+++ isGraphBipartite.cpp
@@ -0,0 +1,50 @@
+// Code copied from GFG verbatim
+// https://www.geeksforgeeks.org/bipartite-graph/
+// C++ program to find out whether a 
+// given graph is Bipartite or not BFS Version
+#include <iostream> 
+#include <queue> 
+#define V 4 
+
+using namespace std; 
+//using BFS 
+bool isBipartite(int G[][V], int src) {
+	int colorArr[V]; 
+	for (int i = 0; i < V; ++i) 
+		colorArr[i] = -1; 
+ 
+	colorArr[src] = 1; 
+ 
+	queue <int> q; 
+	q.push(src); 
+	
+	while (!q.empty())  { 
+		int u = q.front(); 
+		q.pop(); 
+ 
+		if (G[u][u] == 1)  return false; 
+ 
+		for (int v = 0; v < V; ++v) {  
+			if (G[u][v] && colorArr[v] == -1)  {  
+				colorArr[v] = 1 - colorArr[u]; 
+				q.push(v); 
+			}  
+			else if (G[u][v] && colorArr[v] == colorArr[u]) 
+				return false; 
+		} 
+	} 
+	return true; 
+} 
+
+// Driver program to test above function 
+int main() 
+{ 
+	int G[][V] = {{0, 1, 0, 1}, 
+		{1, 0, 1, 0}, 
+		{0, 1, 0, 1}, 
+		{1, 0, 1, 0} 
+	}; 
+
+	isBipartite(G, 0) ? cout << "Yes" : cout << "No"; 
+	return 0; 
+} 
