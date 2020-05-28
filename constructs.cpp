2efec4c4a45b94fa42f18595699b20862c4041ae
Smodi2007 <37974632+Smodi2007@users.noreply.github.com>
2020-05-28 12:37:45 +0530

    Update constructs.cpp

--- constructs.cpp
+++ constructs.cpp
@@ -2,3 +2,23 @@
 To return max of an array.
 //lis[n];
 return *max_element(lis, lis+n); 
+
+
+==============================================
+#include <bits/stdc++.h> 
+using namespace std;
+int main() {
+  
+    vector<vector<int>> nums =  {
+                                    {3,4,5, 0, 0},
+                                    {3,2,6, 0, 0},
+                                    {2,2,1, 0, 0}
+                                }; 
+    printf("%d \n", nums.size());
+    printf("%d \n", nums[0].size());
+    return 0;
+}
+OutPut:
+3 
+5 
+========================================================
